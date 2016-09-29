74d4bb17b6ea62a62d3f91a26babc1def379370c
kristengogol <grkdj11@yahoo.com>
2016-09-28 19:16:12 -0500

    add files via upload

---
+++ class01/ex1/js/main.js
@@ -0,0 +1,11 @@
+// ---------------------------------------
+// Exersize 1: define the following 5 variables, give them values.
+// firstName, lastName, myLove
+// append them to the following css classnames found in the DOM using jQuery
+// .first-name
+// .last-name
+// .my-love
+
+ $('.firstName').append('Kristen');
+ $('.lastName').append('Gogol');
+ $('.myLove').append('cameras');
