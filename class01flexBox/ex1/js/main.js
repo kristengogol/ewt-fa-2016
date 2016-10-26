0a5fe82aaff7bd3a3bf8fad98857e4c106149852
kristengogol <grkdj11@yahoo.com>
2016-10-26 16:23:50 -0500

    Add files via upload

---
+++ class01flexBox/ex1/js/main.js
@@ -0,0 +1,11 @@
+// ---------------------------------------
+// Exersize 1: define the following 5 variables, give them values.
+// firstName, lastName, myLove
+// append them to the following css classnames found in the DOM using jQuery
+// .first-name
+// .last-name
+// .my-love
+
+ $('.first-name').append('Kristen');
+ $('.last-name').append('Gogol');
+ $('.my-love').append('cameras');
